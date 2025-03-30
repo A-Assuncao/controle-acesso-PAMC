481c7518e84c8c8cf0617d121962be2c4082c54e
A-Assuncao <andersongomesrr@hotmail.com>
2025-03-30 16:23:03 -0400

    ajuste

--- controle_acesso/settings.py
+++ controle_acesso/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'sua-chave-secreta-padrao')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
